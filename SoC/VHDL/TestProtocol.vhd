d0aacbabd4cefea98b517cf6e3cef256af7c4ae6
IRDG <53010681+IRDG@users.noreply.github.com>
2020-07-06 13:59:30 -0500

    Test Memory RdWr Successful

--- SoC/VHDL/TestProtocol.vhd
+++ SoC/VHDL/TestProtocol.vhd
@@ -30,7 +30,7 @@ SIGNAL SpySignal        : Spy;
 
 BEGIN
 
-Irq1ShiftInmTest: ENTITY WORK.SoC
+Irq3SpOpTest: ENTITY WORK.SoC
 PORT MAP	  (PeripheralPort   => PeripheralPort,
 				Reset            => Reset,
 				Clk              => Clk,
@@ -38,11 +38,11 @@ PORT MAP	  (PeripheralPort   => PeripheralPort,
 				SpySignal        => SpySignal
 			  );
 
-Clk            <= NOT Clk AFTER   10 ns;
+Clk            <= NOT Clk AFTER  10 ns;
 
-Reset          <= '0'     AFTER   20 ns;
+Reset          <= '0'     AFTER  20 ns;
 
-PeripheralPort <= "0010"  AFTER 2100 ns,
-						"0000"  AFTER 2200 ns;
+PeripheralPort <= "1000"  AFTER  20 ns,
+						"0000"  AFTER 500 ns;
 
 END TestProtocolArch;
